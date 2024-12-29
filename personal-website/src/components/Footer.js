558864b9351bc020c47eba34f41b9843fb2d8d1e
Amin Momin <164680166+aminmomin2@users.noreply.github.com>
2024-12-29 12:54:25 -0600

    Personal website so far

---
+++ personal-website/src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p className="footer-highlight">Contact</p>
+      <h1>I'd Love to Connect With You.</h1>
+      <p>Let me get to know more about you.</p>
+    </footer>
+  )
+}
