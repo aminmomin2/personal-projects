c441dc50f6fafd950274b97b48b6a3341be4c2f4
Amin <aminmomin2006@gmail.com>
2025-01-06 15:11:29 -0600

    Updated about page for personal website

--- personal-website/src/pages/About.js
+++ personal-website/src/pages/About.js
@@ -1,3 +1,19 @@
 export default function About() {
-
+  return (
+    <div className="about-page">
+      <h1>Let me introduce myself</h1>
+      <div className="profile">
+        <img src="/my-image.jpg" alt="Amin Momin"></img>
+        <p>I'm a student currently seeking a job as a software engineer. I'm also University of Texas At Austin class of 2024 B.S. My primary interests are software development, machine learning, and artifial intelligence.</p>
+      </div>
+      <div className="personal-info">
+        <h3>PROFILE</h3>
+        <p>Currently I am developing my skills in full stack engineer. Currently I am focusing on React, NodeJS, and Firebase. If you think I am a good fit, please contact me via <a href="mailto:aminmomin2006@gmail.com" className="email">aminmomin2006@gmail.com</a></p>
+        <h3>FULL NAME:</h3>
+        <h4>AMINUDDIN MOMIN</h4>
+        <h3>EMAIL:</h3>
+        <h4>aminmomin2006@gmail.com</h4>
+      </div>
+    </div>
+  )
 }
