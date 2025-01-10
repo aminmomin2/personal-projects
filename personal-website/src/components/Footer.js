cc480b34b6cc932317cf480637b966d6fcca44e0
Amin <aminmomin2006@gmail.com>
2025-01-10 11:49:59 -0600

    Delete personal website

--- personal-website/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <p className="footer-highlight">Contact</p>
-      <h1>I'd Love to Connect With You.</h1>
-      <p>Let me get to know more about you.</p>
-    </footer>
-  )
-}
